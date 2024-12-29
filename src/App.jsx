a53d3e615af87dfffc6008eb7840efedd804a62b
marufmks <marufmks5556@gmail.com>
2024-12-29 12:01:39 +0600

    added framer motion

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Technologies from "./components/Technologies";
+import Experience from "./components/Experience";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 function App() {
   return (
     <>
@@ -11,6 +15,10 @@ function App() {
           <Navbar />
           <Hero />
           <About />
+          <Technologies />
+          <Experience/>
+          <Projects/>
+          <Contact/>
         </div>
       </div>
     </>
---
+++ src/components/Technologies.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+import {
+  FaReact,
+  FaPhp,
+  FaWordpress,
+  FaDatabase,
+  FaLaravel,
+} from "react-icons/fa";
+import { SiTailwindcss } from "react-icons/si";
+
+import { motion } from "framer-motion";
+
+const iconVariants = (duration) => ({
+  initial: { y: -10 },
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse",
+    },
+  },
+});
+const Technologies = () => {
+  return (
+    <div className="border-b border-neutral-800 pb-24">
+      <motion.h1
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Technologies
+      </motion.h1>
+      <motion.div
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-wrap items-center justify-center gap-4"
+      >
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaReact className="text-7xl text-[#61DAFB]" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiTailwindcss className="text-7xl text-[#06B6D4]" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(2)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaLaravel className="text-7xl text-[#FF2D20]" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(6)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaWordpress className="text-7xl text-[#21759B]" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(4)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaDatabase className="text-7xl text-[#336791]" />
+        </motion.div>
+      </motion.div>
+    </div>
+  );
+};
+
+export default Technologies;
